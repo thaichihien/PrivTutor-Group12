06fb49f8f96f2c1407dff6041952134ad82ee008
Nguyen Manh Hung <81510821+Hung369@users.noreply.github.com>
2022-12-26 11:34:38 +0700

    Update posgreSQL connection with node.js

--- main.js
+++ main.js
@@ -1,1 +1,18 @@
-console.log("Hello World")
+const { Pool, Client } = require('pg')
+ 
+const pool = new Pool({
+    user: 'postgres',
+    host: 'localhost',
+    database: 'privtutor_db',
+    password: 'your password',
+    port: 5432,
+})
+
+pool.connect()
+   
+pool.query('SELECT * FROM Student', (err, res) => {
+    console.log(err, res)
+    pool.end()
+})
+
+// your password là mật khẩu kết nối vs Postgres của mỗi máy.
